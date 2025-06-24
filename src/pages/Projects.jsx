4045b1429b0dd70826f70bf4b54f686e8ec9a832
WYZEPRINCE <adelaniadelaja@gmail.com>
2025-06-24 15:03:01 +0100

    slight modification

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -42,7 +42,9 @@ const Projects = () => {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
-            <button>VIEW PROJECT →</button>
+            <Link to="/project1">
+              <button>VIEW PROJECT →</button>
+            </Link>
           </div>
         </div>
       </section>
